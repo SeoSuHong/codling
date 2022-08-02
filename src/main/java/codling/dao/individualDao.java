eb4c21ac7698c0f51f486da4800d594087a8acdd
Seosuhong <tbgkdntm@naver.com>
2022-08-02 11:41:40 +0900

    [update] 파일 오류 수정

--- src/main/java/codling/dao/individualDao.java
+++ src/main/java/codling/dao/individualDao.java
@@ -5,20 +5,51 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-public class individualDao {
-    final static String DB_URL = "jdbc:mysql://localhost:3306/codling";
-    final static String DB_NAME = "codling";
-    final static String DB_PASSWORD = "1234";
-    
-    static Connection conn;
-    static PreparedStatement pstmt;
-    static ResultSet rs;
-    
-    protected Connection getConnection() throws Exception {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(DB_URL, DB_NAME, DB_PASSWORD);
-            return conn;
-    }
-    
-    
+import codling.identity.Individual;
+
+public class IndividualDao {
+	final static String DB_URL = "jdbc:mysql://localhost:3306/codling";
+	final static String DB_NAME = "codling";
+	final static String DB_PASSWORD = "1234";
+	
+	static Connection conn;
+	static PreparedStatement pstmt;
+	static ResultSet rs;
+	
+	protected Connection getConnection() throws Exception {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(DB_URL, DB_NAME, DB_PASSWORD);
+			return conn;
+	}
+	
+	// 개인회원 정보
+	public Individual getIndividual(String id) {
+		Individual individual = null;
+		String query = "SELECT * FROM individual WHERE id = ?";
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				String password = rs.getString("password");
+				String name = rs.getString("name");
+				String birth = rs.getString("birth");
+				String gender = rs.getString("gender");
+				String email = rs.getString("email");
+				String phone = rs.getString("phone");
+				String address = rs.getString("address");
+				
+				individual = new Individual(id, password, name, birth, gender, email, phone, address, "");
+			}
+			
+			rs.close();
+			pstmt.close();
+			conn.close();
+		} catch(Exception e) {
+			System.out.println("getIndiInfo Error : " + e.getMessage());
+		}
+		return individual;
+	}
 }
