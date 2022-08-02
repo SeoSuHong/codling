b357366b60fd28bf84c7b371be828004d1fd6bd1
Woojin Lim <dnwls4031@naver.com>
2022-08-02 09:57:12 +0900

    [update] 병합 충돌 에러 수정

--- src/main/java/codling/controller/Job_accountmentServlet.java
+++ src/main/java/codling/controller/Job_accountmentServlet.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import codling.dao.CorporationDao;
-<<<<<<< HEAD
 import codling.identity.Corporation;
 import codling.identity.Field;
 import codling.identity.JobOpening;
@@ -30,23 +29,4 @@ public class Job_accountmentServlet extends HttpServlet {
 		
 		request.getRequestDispatcher("job_accountment.jsp").forward(request, response);
 	}
-=======
-import codling.identity.Field;
-import codling.identity.JobOpening;
-
-@WebServlet("/Job_accountmentServlet")
-public class Job_accountmentServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        CorporationDao corpDao = new CorporationDao();
-        
-        JobOpening jobOpening = corpDao.getJobOpening("ezen");
-        int no = jobOpening.getNo();
-        Field field = corpDao.getField(no);
-        
-        request.setAttribute("jobOpening", jobOpening);
-        request.setAttribute("field", field);
-        
-        request.getRequestDispatcher("job_accountment.jsp").forward(request, response);
-    }
->>>>>>> 우진
 }
