e30c99f267c74b132bf88b80c0f8c5683e190148
Woojin Lim <dnwls4031@naver.com>
2022-08-03 15:44:29 +0900

    서블릿 생성중..

--- src/main/java/codling/controller/company_modifyServlet.java
+++ src/main/java/codling/controller/company_modifyServlet.java
@@ -6,6 +6,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import codling.dao.CorporationDao;
+import codling.identity.Corporation;
 
 
 @WebServlet("/company_modify")
@@ -13,10 +17,19 @@ public class company_modifyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute(getServletName(), response);
+		HttpSession session = request.getSession();
+		String id = (String)session.getAttribute("corpId");
+		
+		CorporationDao dao = new CorporationDao();
+		Corporation corporation = dao.getCorporation(id);
+		
+		request.setAttribute("corporation", corporation);
+		request.getRequestDispatcher("/WEB/INF/corporation/company_modify.jsp").forward(request, response);
 	}
-
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String id = (String)session.getAttribute("corpId");
 		
 	}
 
