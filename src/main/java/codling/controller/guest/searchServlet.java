8950532ae53421032174a3fcf5c1d1bb3bb9e807
Seosuhong <tbgkdntm@naver.com>
2022-09-01 15:58:13 +0900

    [add]search 페이지 추가

---
+++ src/main/java/codling/controller/guest/searchServlet.java
@@ -0,0 +1,46 @@
+package codling.controller.guest;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import codling.dao.CorporationDao;
+import codling.dao.InformationDao;
+import codling.identity.Announcement;
+
+@WebServlet("/index")
+public class searchServlet extends HttpServlet{
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String indiId = (String)session.getAttribute("indiId");
+		String corpId = (String)session.getAttribute("corpId");
+		
+		InformationDao infoDao = new InformationDao();
+		if(indiId != null) {
+			Map<String, String> map = infoDao.getIndiName(indiId);
+			String indiName = map.get(indiId);
+			request.setAttribute("indiName", indiName);
+		} else if(corpId != null) {
+			Map<String, String> map = infoDao.getCorpName(corpId);
+			String corpName = map.get(corpId);
+			request.setAttribute("corpName", corpName);
+		}
+		
+		CorporationDao corpDao = new CorporationDao();
+		
+		List<String> fieldNames = corpDao.getAllFieldName();
+		ArrayList<Announcement> announcement = corpDao.indexContents();
+		
+		request.setAttribute("fieldNames", fieldNames);
+		request.setAttribute("announcement", announcement);
+		request.getRequestDispatcher("/WEB-INF/guest/search.jsp").forward(request, response);
+	}
+}
--- src/main/webapp/WEB-INF/guest/search.jsp
+++ src/main/webapp/WEB-INF/guest/search.jsp
@@ -12,9 +12,9 @@
     <link href="../../img/headlogo.png" rel="shortcut icon" type="image/x-icon">
     <script src="js/jquery-3.6.0.min.js"></script>
     <script src="js/jquery-ui.min.js"></script>
-    <script src="js/index_app.js"></script>
+    <script src="js/search.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/index_style.css">
+    <link rel="stylesheet" href="css/search.css">
 </head>
 <body>
 
